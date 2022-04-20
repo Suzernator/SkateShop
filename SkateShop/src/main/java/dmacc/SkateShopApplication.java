17ddff9375725279bcc15daccd64bc9dd3a7780f
Suzette Senephansiri <93677348+Suzernator@users.noreply.github.com>
2022-04-20 12:45:39 -0500

    commit
    application

--- SkateShop/src/main/java/dmacc/SkateShopApplication.java
+++ SkateShop/src/main/java/dmacc/SkateShopApplication.java
@@ -1,7 +1,17 @@
 package dmacc;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import dmacc.beans.Customer;
+import dmacc.controller.BeanConfiguration;
+import dmacc.repository.CustomerRepository;
 
 @SpringBootApplication
 public class SkateShopApplication {
@@ -9,5 +19,18 @@ public class SkateShopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SkateShopApplication.class, args);
 	}
+	
+	@Autowired
+	CustomerRepository repo;
+	
+	public void run(String...args)throws Exception {
+		ApplicationContext appContext = new AnnotationConfigApplicationContext(BeanConfiguration.class);
 
+		List<Customer> allCustomers = repo.findAll();
+		for(Customer customers: allCustomers) {
+			System.out.println(customers.toString());
+		}
+		
+		((AbstractApplicationContext) appContext).close();
+	}
 }

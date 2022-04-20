7d962aaa18d9e9e4f53d7945ba2b30de18f53672
chasevanblair <61804205+chasevanblair@users.noreply.github.com>
2022-04-19 20:19:00 -0500

    bean config

--- SkateShop/src/main/java/dmacc/controller/BeanConfiguration.java
+++ SkateShop/src/main/java/dmacc/controller/BeanConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import dmacc.beans.Customer;
+import dmacc.beans.Inventory;
+import dmacc.beans.Item;
+import dmacc.beans.Order;
 
 @Configuration
 public class BeanConfiguration {
@@ -12,6 +15,21 @@ public class BeanConfiguration {
 	public Customer contact() {
 		Customer bean = new Customer();
 		return bean;
+	}
+	@Bean
+	public Inventory inventory() {
+		Inventory inventory = new Inventory();
+		return inventory;
+	}
+	@Bean
+	public Order order() {
+		Order order = new Order();
+		return order;
+	}
+	@Bean
+	public Item item() {
+		Item item = new Item();
+		return item;
 	}
 
 //
